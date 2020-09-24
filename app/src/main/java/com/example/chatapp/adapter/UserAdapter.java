5c3ab26746e85fa60b23f008f20f5e7ce9af2825
Eganin <egorzaharin12@gmail.com>
2020-09-24 19:51:56 +0500

    add user adapter

--- app/src/main/java/com/example/chatapp/adapter/UserAdapter.java
+++ app/src/main/java/com/example/chatapp/adapter/UserAdapter.java
@@ -1,4 +1,85 @@
 package com.example.chatapp.adapter;
 
-public class UserAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatapp.R;
+import com.example.chatapp.model.User;
+
+import java.util.ArrayList;
+
+public class UserAdapter
+        extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+    private ArrayList<User> userArrayList;
+    private OnUserClickListener listener;
+
+    public interface OnUserClickListener {
+        void onUserClick(int position);
+    }
+
+    public UserAdapter(ArrayList<User> userArrayList) {
+        this.userArrayList = userArrayList;
+    }
+
+    public UserAdapter() {
+
+    }
+
+    public void setOnClickListener(OnUserClickListener listener){
+        this.listener=listener;
+    }
+
+    @NonNull
+    @Override
+    public UserAdapter.UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.user_item , parent , false);
+
+        UserViewHolder userViewHolder = new UserViewHolder(view , listener);
+        return userViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserAdapter.UserViewHolder holder, int position) {
+        User currentUser = userArrayList.get(position);
+        holder.imageViewAvatarUser.setImageResource(currentUser.getAvatarMockUpResource());
+        holder.textViewUserName.setText(currentUser.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return userArrayList.size();
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView imageViewAvatarUser;
+        public TextView textViewUserName;
+
+        public UserViewHolder(@NonNull View itemView, final OnUserClickListener listener) {
+            super(itemView);
+
+            imageViewAvatarUser = itemView.findViewById(R.id.imageViewAvatarUser);
+            textViewUserName = itemView.findViewById(R.id.textViewUserName);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(listener != null){
+                        int position = getAdapterPosition();// get position
+                        if(position != RecyclerView.NO_POSITION){
+                            listener.onUserClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
 }
--- app/src/main/java/com/example/chatapp/model/User.java
+++ app/src/main/java/com/example/chatapp/model/User.java
@@ -5,15 +5,17 @@ public class User {
     private String name;
     private String email;
     private String id;
+    private int avatarMockUpResource;
 
     public User() {
 
     }
 
-    public User(String name, String email, String id) {
+    public User(String name, String email, String id , int avatarMockUpResource) {
         this.name = name;
         this.email = email;
         this.id = id;
+        this.avatarMockUpResource=avatarMockUpResource;
     }
 
     public String getEmail() {
@@ -38,5 +40,13 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAvatarMockUpResource() {
+        return avatarMockUpResource;
+    }
+
+    public void setAvatarMockUpResource(int avatarMockUpResource) {
+        this.avatarMockUpResource = avatarMockUpResource;
     }
 }
